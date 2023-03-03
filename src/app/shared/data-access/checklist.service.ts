23d81692bfbea4ef8114e1fbcdaf5d1bcd9281a7
Joshua Morony <joshua.morony@gmail.com>
2023-03-03 14:44:26 +1030

    add checklist service

---
+++ src/app/shared/data-access/checklist.service.ts
@@ -0,0 +1,73 @@
+import { computed, effect, Injectable, signal } from '@angular/core';
+import { ChecklistItemService } from '../../checklist/data-access/checklist-item.service';
+import { AddChecklist, Checklist } from '../interfaces/checklist';
+import { StorageService } from './storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChecklistService {
+  private checklists = signal<Checklist[]>([]);
+
+  constructor(
+    private storageService: StorageService,
+    private checklistItemService: ChecklistItemService
+  ) {}
+
+  load() {
+    const checklists = this.storageService.loadChecklists();
+    this.checklists.set(checklists);
+
+    effect(() => {
+      this.storageService.saveChecklists(this.checklists())
+    })
+  }
+
+  getChecklists() {
+    return this.checklists();
+  }
+
+  getChecklistById(id: string) {
+    return computed(() => this.getChecklists().find((checklist) => checklist.id === id))
+  }
+
+  add(checklist: AddChecklist) {
+    const newChecklist = {
+      ...checklist,
+      id: this.generateSlug(checklist.title),
+    };
+
+    this.checklists.mutate((checklists) => checklists.push(newChecklist))
+  }
+
+  remove(id: string) {
+    this.checklistItemService.removeAllItemsForChecklist(id);
+    this.checklists.update((checklists) => checklists.filter((checklist) => checklist.id !== id))
+  }
+
+  update(id: string, editedData: AddChecklist) {
+    this.checklists.update((checklists) => checklists.map((checklist) =>
+      checklist.id === id
+        ? { ...checklist, title: editedData.title }
+        : checklist
+    ))
+  }
+
+  private generateSlug(title: string) {
+    // NOTE: This is a simplistic slug generator and will not handle things like special characters.
+    let slug = title.toLowerCase().replace(/\s+/g, '-');
+
+    // Check if the slug already exists
+    const matchingSlugs = this.checklists().find(
+      (checklist) => checklist.id === slug
+    );
+
+    // If the title is already being used, add a string to make the slug unique
+    if (matchingSlugs) {
+      slug = slug + Date.now().toString();
+    }
+
+    return slug;
+  }
+}
+ 
