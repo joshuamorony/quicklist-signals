b7bd40c8fe065f116b85c950a9a0778464f9440c
Joshua Morony <joshua.morony@gmail.com>
2023-03-06 12:44:32 +1030

    checklist item header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Route } from "@angular/router";
 
 export const routes: Route[] = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.component')
+    path: "home",
+    loadComponent: () => import("./home/home.component"),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  }
-]
+    path: "checklist/:id",
+    loadComponent: () => import("./checklist/checklist.component"),
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+];
